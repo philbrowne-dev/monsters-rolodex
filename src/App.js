9f3f1769a99d2bb16afc7423dc33f301c1d800a2
Phil Browne <phil.browne@millbrookhealthcare.co.uk>
2020-10-29 09:42:55 +0000

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
+//import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-class App extends Component{
+class App extends React.Component{
   constructor(){
     super();
     this.state ={
-      username: 'Hello Phil Browne'
+      username: 'Phil Browne',
+      monsters: [],
+      searchfield: ''
     }
+    
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json(response => response.json())
+    .then(users => this.setState({monsters: users}))
+    )
+
+  }
+
+  handleChange = (e) => {
+    this.setState({searchfield: e.target.value})
+  }
+
   render(){
+    const { monsters, searchfield} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchfield.toLowerCase())
+    )
+
     return (
           <div className="App">
-            <header className="App-header">
-    <p>{this.state.username}</p>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <button onClick={() => this.setState({username: 'Hello Phil'})}>Change Text</button>
-            </header>
+            <h1>Monsters Rolodex</h1>
+            <SearchBox 
+            placeholder='Search Monsters'
+            handleChange={this.handleChange}
+            />
+
+            <CardList monsters={filteredMonsters}>
+           
+            </CardList>
+        
           </div>
       
     )
